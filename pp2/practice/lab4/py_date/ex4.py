578ec457b63d080608b2f8fc6df9581e4750e27f
Aiym Kairbayeva <ayim0812@gmail.com>
2024-02-22 00:38:23 +0600

    lab4

--- pp2/practice/lab4/py_date/ex4.py
+++ pp2/practice/lab4/py_date/ex4.py
@@ -1,5 +1,5 @@
 from datetime import *
 day1 = datetime.now()
 day2 = datetime(2024, 1, 12)
-dif = day1 - day2
-print(dif.total_seconds())
+difference = day1 - day2
+print(difference.total_seconds())
---
+++ pp2/practice/lab4/py_generators/ex1.py
@@ -0,0 +1,3 @@
+def sqrt(n):
+    for i in range(n+1):
+        return i*i 
