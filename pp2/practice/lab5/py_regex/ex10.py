49f043527f5268172b15ff076b265171774abb04
Aiym Kairbayeva <ayim0812@gmail.com>
2024-02-24 17:42:50 +0600

    5lab chto smogla to i sdelala

--- pp2/practice/lab5/py_regex/ex10.py
+++ pp2/practice/lab5/py_regex/ex10.py
@@ -1,1 +1,6 @@
 #Write a Python program to convert a given camel case string to snake case.
+from re import *
+bucvy_str = str(input())
+
+x = sub(r".[A-Z]", lambda a:f"{a.group().lower()[0]}_{a.group().lower()[1]}", bucvy_str)
+print(x)
